0836083f4b2f98bf55721c81ea9c8f3b0e064fe8
Carlos Evelo <carlosevelo96@gmail.com>
2023-02-28 16:05:49 -0700

    no api key

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -18,7 +18,7 @@ struct CoinManager {
     var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "3692C70B-2C13-4CB5-A3C9-B77C2CAA13EB"
+    let apiKey = "API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
